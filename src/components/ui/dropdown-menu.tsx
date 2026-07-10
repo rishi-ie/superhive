fc0416b5b69e7a8885b8bcbb42c1660b00510dee
rishi-ie <rishisidharda.n@gmail.com>
2026-07-10 20:53:38 +0530

    refactor(icons): migrate dropdown-menu.tsx to Phosphor

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -2,8 +2,8 @@ import * as React from "react"
 import { DropdownMenu as DropdownMenuPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
-import { HugeiconsIcon } from "@/components/ui/icon";
-import { CheckIcon, ArrowRight01Icon } from "@hugeicons/core-free-icons";
+import { Icon } from "@/components/ui/icon";
+import { CheckIcon, CaretRightIcon } from "@phosphor-icons/react";
 
 function DropdownMenu({
   ...props
@@ -105,7 +105,7 @@ function DropdownMenuCheckboxItem({
         data-slot="dropdown-menu-checkbox-item-indicator"
       >
         <DropdownMenuPrimitive.ItemIndicator>
-          <HugeiconsIcon icon={CheckIcon}
+          <Icon icon={CheckIcon}
           />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
@@ -148,7 +148,7 @@ function DropdownMenuRadioItem({
         data-slot="dropdown-menu-radio-item-indicator"
       >
         <DropdownMenuPrimitive.ItemIndicator>
-          <HugeiconsIcon icon={CheckIcon}
+          <Icon icon={CheckIcon}
           />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
@@ -231,7 +231,7 @@ function DropdownMenuSubTrigger({
       {...props}
     >
       {children}
-      <HugeiconsIcon icon={ArrowRight01Icon} className="ml-auto" />
+      <Icon icon={CaretRightIcon} className="ml-auto" />
     </DropdownMenuPrimitive.SubTrigger>
   )
 }
