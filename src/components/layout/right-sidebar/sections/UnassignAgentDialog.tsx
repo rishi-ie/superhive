8b4863f47f50130c0df73724bc058a69d7abe6e7
rishi-ie <rishisidharda.n@gmail.com>
2026-07-12 12:57:12 +0530

    feat(ui): implement UnassignAgentDialog body

--- src/components/layout/right-sidebar/sections/UnassignAgentDialog.tsx
+++ src/components/layout/right-sidebar/sections/UnassignAgentDialog.tsx
@@ -22,9 +22,10 @@ export function UnassignAgentDialog(props: UnassignAgentDialogProps) {
 			<AlertDialogContent>
 				<AlertDialogHeader>
 					<AlertDialogTitle>Unlink running agent?</AlertDialogTitle>
-					<AlertDialogDescription>
-						{props.agentName ?? 'This agent'} placeholder description.
-					</AlertDialogDescription>
+<AlertDialogDescription>
+	{props.agentName ?? 'This agent'} is currently running. It will keep running but
+	won't be a member of this project.
+</AlertDialogDescription>
 				</AlertDialogHeader>
 				<AlertDialogFooter>
 					<AlertDialogCancel onClick={props.onCancel}>Cancel</AlertDialogCancel>
