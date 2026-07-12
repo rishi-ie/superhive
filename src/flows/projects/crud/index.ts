1e92be27ab304aa9d113850d5cb252c15517725f
rishi-ie <rishisidharda.n@gmail.com>
2026-07-12 12:53:40 +0530

    feat(flows): export new project flows from barrel

--- src/flows/projects/crud/index.ts
+++ src/flows/projects/crud/index.ts
@@ -2,3 +2,6 @@ export { createProject } from './create-project';
 export { deleteProject } from './delete-project';
 export { listProjects } from './list-projects';
 export { loadProject } from './load-project';
+export { loadProjectTeam, loadUnassignedAgents } from './load-project-team';
+export { assignAgentToProject } from './assign-agent-to-project';
+export { removeAgentFromProject } from './remove-agent-from-project';
