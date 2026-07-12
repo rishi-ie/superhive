c8b9ba5dc42507dcbbe7aa978c15b6bf3431ba61
rishi-ie <rishisidharda.n@gmail.com>
2026-07-12 12:57:01 +0530

    feat(ui): scaffold UnassignAgentDialog

---
+++ src/components/layout/right-sidebar/sections/UnassignAgentDialog.tsx
@@ -0,0 +1,36 @@
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+
+interface UnassignAgentDialogProps {
+	open: boolean;
+	agentName: string | null;
+	onConfirm: () => void;
+	onCancel: () => void;
+}
+
+export function UnassignAgentDialog(props: UnassignAgentDialogProps) {
+	return (
+		<AlertDialog open={props.open} onOpenChange={(o) => { if (!o) props.onCancel(); }}>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Unlink running agent?</AlertDialogTitle>
+					<AlertDialogDescription>
+						{props.agentName ?? 'This agent'} placeholder description.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel onClick={props.onCancel}>Cancel</AlertDialogCancel>
+					<AlertDialogAction onClick={props.onConfirm}>Unlink anyway</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
