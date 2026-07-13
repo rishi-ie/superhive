b9e5ac2cf380f79eb86795c6826e6349993a5def
rishi-ie <rishisidharda.n@gmail.com>
2026-07-14 00:35:50 +0530

    P3.2.3: Wire react-markdown to MarkdownPart

--- src/pages/agent-chat/components/message-parts/MarkdownPart.tsx
+++ src/pages/agent-chat/components/message-parts/MarkdownPart.tsx
@@ -1,7 +1,14 @@
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
 interface MarkdownPartProps {
   source: string
 }
 
 export function MarkdownPart({ source }: MarkdownPartProps) {
-  return <div>{source}</div>
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      {source}
+    </ReactMarkdown>
+  )
 }
